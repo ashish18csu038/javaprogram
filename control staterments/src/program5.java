6f9481afb96e8713fcc56afdcfa421313b0253be
ashish18csu038 <ashish18csu038@ncuindia.edu>
2019-01-11 01:14:59 +0530

    Initial commit

---
+++ control staterments/src/program5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class program5
+{
+	public static void main(String[] args)
+	{
+		int a=4;
+		int b=2;
+		int Sub;
+		int Mul;
+		int Add;
+		int Div;
+		int Mod;
+		Sub=a-b;
+		Mul=a*b;
+		Add=a+b;
+		Div=a/b;
+		Mod=a%b;
+		System.out.println("Subtraction :"+Sub);
+		System.out.println("Mutiplication :"+Mul);
+		System.out.println("Subtraction :"+Sub);
+		System.out.println("Division :"+Div);
+		System.out.println("Modulus :"+Mod);
+		
+
+	}
+}
